9cc27e79ac0de6d35bec71e165a4b498ec8cbf5b
felixtoo48 <felixtoo48@gmail.com>
2023-09-27 14:50:08 +0300

    adding <0 digit variation

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,16 +7,19 @@ from math import factorial
 
 def pascal_triangle(n):
     """ prints pascal's triangle with n rows"""
-    for i in range(n):
-        """loop to get leading spaces"""
-        for j in range(n-i+1):
-            print(end="")
+    if n <= 0:
+        return []
 
-        """loop to get elements for row i"""
+    """ initialize rezult list"""
+    result = []
+
+    for i in range(n):
+        """initialize row results"""
         row = []
+
         for j in range(i+1):
             """nCr = n!/((n-r)!*r!)"""
             row.append(factorial(i) // (factorial(j) * factorial(i - j)))
+        result.append(row)
 
-        print("[", end="")
-        print(*row, sep=', ', end=']\n')
+    return result
