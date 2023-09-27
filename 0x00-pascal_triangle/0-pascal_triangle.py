a3554df30422b171a6ab9c412b59ff3ef2e155dd
felixtoo48 <felixtoo48@gmail.com>
2023-09-27 11:02:23 +0300

    pascals triangle definition

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+pascal triangle defination file
+"""
+from math import factorial
+
+
+def pascal_triangle(n):
+    """ prints pascal's triangle with n rows"""
+    for i in range(n):
+        """loop to get leading spaces"""
+        for j in range(n-i+1):
+            print(end=" ")
+
+        """loop to get elements for row i"""
+        for j in range(i+1):
+            """nCr = n!/((n-r)!*r!)"""
+            print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
+
+        """print each row in a new line"""
+        print("\n")
