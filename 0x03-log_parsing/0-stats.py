56e54bf794e907c964ef8dc9d061580bc5fd1919
felixtoo48 <felixtoo48@gmail.com>
2023-10-19 20:07:58 +0300

    err by the checker

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,63 +1,48 @@
-#!/usr/bin/env python3
-""" log parsing algorithm """
+#!/usr/bin/python3
+"""
+module contains a script that reads stdin line by line and computes metrics
+"""
 import sys
-import re
 
 
-"""Define the regular expression pattern to match the desired log format"""
-ip_pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
-log_date_pattern = r'\[([^\]]+)] "GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)'
-log_pattern = ip_pattern + r' - ' + log_date_pattern
-
-"""Initializing variables to keep track of statistics"""
-file_sizes = []
-status_code_counts = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
+status_codes = {
+    "200": 0,
+    "301": 0,
+    "400": 0,
+    "401": 0,
+    "403": 0,
+    "404": 0,
+    "405": 0,
+    "500": 0
 }
 
-try:
-    line_count = 0
-    total_file_size = 0
+file_size = 0
 
-    for line in sys.stdin:
-        """ removing leading and trailing whitespace from line"""
-        line = line.rstrip()
 
-        """matching line to the desired input format"""
-        match = re.match(log_pattern, line)
+def print_metrics():
+    """prints of the logs"""
+    print("File size: {}".format(file_size))
+    for status in sorted(status_codes.keys()):
+        if status_codes[status]:
+            print("{}: {}".format(status, status_codes[status]))
 
-        if match:
-            """ Extract ip, date, status code, file size"""
-            ip_address, date, status_code, file_size = match.groups()
 
-            total_file_size += int(file_size)
-
-            file_sizes.append(int(file_size))
-
-            if int(status_code) in status_code_counts:
-                status_code_counts[int(status_code)] += 1
-
-            line_count += 1
-
-            # Print statistics after every 10 lines"""
-            if line_count % 10 == 0:
-                print(f'File size: {total_file_size}')
-                for code, count in sorted(status_code_counts.items()):
-                    if count > 0:
-                        print(f'{code}: {count}')
-
-except KeyboardInterrupt:
-    print("")
-
-# Print the final statistics after processing all lines"""
-print(f'File size: {total_file_size}')
-for code, count in sorted(status_code_counts.items()):
-    if count > 0:
-        print(f'{code}: {count}')
+if __name__ == "__main__":
+    count = 0
+    try:
+        for line in sys.stdin:
+            try:
+                elems = line.split()
+                file_size += int(elems[-1])
+                if elems[-2] in status_codes:
+                    status_codes[elems[-2]] += 1
+            except Exception:
+                pass
+            if count == 9:
+                print_metrics()
+                count = -1
+            count += 1
+    except KeyboardInterrupt:
+        print_metrics()
+        raise
+    print_metrics()
