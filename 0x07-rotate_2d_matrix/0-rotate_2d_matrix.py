35b0e5cb3344bd3affc1437ba07b4b280a440ee0
felixtoo48 <felixtoo48@gmail.com>
2023-11-14 20:48:29 +0300

    changing transposing of the matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-""" Function rotating 2D matrix 90 degree clockwise"""
-
-N = 3
+""" Function rotating 2D matrix 90 degrees clockwise"""
 
 
 def rotate_2d_matrix(matrix):
-    """ function to rotate 2D matrix by 90 degrees """
-    global N
+    """Rotate 2D matrix by 90 degrees clockwise in-place."""
+    n = len(matrix)
 
-    for j in range(N):
-        for i in range(N - 1, -1, -1):
-            print(matrix[i][j], end=' ')
-        print()
+    # Reverse the matrix, row-wise
+    for i in range(n // 2):
+        matrix[i], matrix[n - 1 - i] = matrix[n - 1 - i], matrix[i]
+
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
